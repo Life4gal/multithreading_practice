d9f1561a707ed69b338939cf4796c6015d0cf1f2
life4gal <life4gal@gmail.com>
2021-06-04 10:29:46 +0800

    去除file_finder冗余

--- work/file_finder.cpp
+++ work/file_finder.cpp
@@ -4,6 +4,25 @@
 
 #include "error_logger.hpp"
 
+namespace {
+	template<typename Iterator>
+	void do_find(std::vector<std::string> &out, const std::string &suffix, Iterator begin, Iterator end = {}) {
+		bool need_extension = !suffix.empty();
+		for (; begin != end; ++begin) {
+			auto p = begin->path();
+
+			if (need_extension) {
+				if (p.has_extension() && p.extension().string() == suffix) {
+					out.emplace_back(p.string());
+				}
+			} else {
+				out.emplace_back(p.string());
+			}
+			log_to_file("Info", __PRETTY_FUNCTION__, "Found file: " + p.string());
+		}
+	}
+}// namespace
+
 std::vector<std::string> file_finder::get_files_in_path(const std::string &path, const std::string &suffix, bool recursive) {
 	std::vector<std::string> ret{};
 
@@ -11,35 +30,10 @@ std::vector<std::string> file_finder::get_files_in_path(const std::string &path,
 		namespace fs = boost::filesystem;
 
 		fs::path dir_path{path};
-		bool	 need_extension = !suffix.empty();
-
-		// todo: 去除冗余，C++11不支持lambda参数类型使用占位符
 		if (recursive) {
-			for (auto it = fs::recursive_directory_iterator{dir_path}; it != fs::recursive_directory_iterator{}; ++it) {
-				auto p = it->path();
-
-				if (need_extension) {
-					if (p.has_extension() && p.extension().string() == suffix) {
-						ret.emplace_back(p.string());
-					}
-				} else {
-					ret.emplace_back(p.string());
-				}
-				log_to_file("Info", __PRETTY_FUNCTION__, "Found file: " + p.string());
-			}
+			do_find(ret, suffix, fs::recursive_directory_iterator{dir_path});
 		} else {
-			for (auto it = fs::directory_iterator{dir_path}; it != fs::directory_iterator{}; ++it) {
-				auto p = it->path();
-
-				if (need_extension) {
-					if (p.has_extension() && p.extension().string() == suffix) {
-						ret.emplace_back(p.string());
-					}
-				} else {
-					ret.emplace_back(p.string());
-				}
-				log_to_file("Info", __PRETTY_FUNCTION__, "Found file: " + p.string());
-			}
+			do_find(ret, suffix, fs::directory_iterator{dir_path});
 		}
 	} else {
 		log_to_file("Error", __PRETTY_FUNCTION__, "Empty path");
